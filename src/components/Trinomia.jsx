3e232725db5058897749c495c5270f0943d95710
stn <smrcouto88@gmail.com>
2022-10-16 02:21:27 -0300

    fontsource | classnames again

--- src/components/Trinomia.jsx
+++ src/components/Trinomia.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Confetti from "react-dom-confetti";
 export default function Trinomia() {
   return (
-    <div class="flex flex-col items-center justify-center w-full py-20 overflow-hidden border-b dark:border-gray-700">
-      <p class="w-full mb-6 font-mono text-center select-all dark:text-white">
+    <div className="flex flex-col items-center justify-center w-full py-20 overflow-hidden border-b dark:border-gray-700">
+      <p className="w-full mb-6 font-mono text-center select-all dark:text-white">
       santuan.bot@gmail.com
       </p>
       <button
@@ -13,9 +13,9 @@ export default function Trinomia() {
           setTimeout(() => setIsCopied(false), 3000);
         }}
       >
-        <div class="flex justify-center py-1 mx-auto space-x-1 text-3xl group ">
+        <div className="flex justify-center py-1 mx-auto space-x-1 text-3xl group ">
           <svg
-            class="duration-300 dark:text-white group-hover:text-blue-500"
+            className="duration-300 dark:text-white group-hover:text-blue-500"
             width="32"
             height="32"
             viewBox="0 0 16 16"
@@ -23,7 +23,7 @@ export default function Trinomia() {
             <path fill="currentColor" d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
           </svg>
           <svg
-            class="duration-300 dark:text-white group-hover:text-red-500"
+            className="duration-300 dark:text-white group-hover:text-red-500"
             width="32"
             height="32"
             viewBox="0 0 16 16"
@@ -31,14 +31,14 @@ export default function Trinomia() {
             <circle cx="8" cy="8" r="8" fill="currentColor" />
           </svg>
           <svg
-            class="duration-300 dark:text-white scale-105 group-hover:text-yellow-500"
+            className="duration-300 dark:text-white scale-105 group-hover:text-yellow-500"
             width="32"
             height="32"
             viewBox="0 0 16 16"
           >
             <path
               fill="currentColor"
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"
             />
           </svg>
